dfc158dc5045d10ac244a8bec1c2ed19a3a4fab6
jskimweb <jskimweb@gmail.com>
2021-09-08 18:13:09 +0900

    Complete the '.latest'

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 window.onload = function() {
 	new Swiper('.sw-latest', {
 		slidesPerView: "auto",
-        spaceBetween: 25,
-		// slidesPerGroup: 3,
-		on: {
-			slideChange: function() {
-				changeGroup(this.realIndex);
-			}
-		}
+		slidesPerGroup: 3,
+		pagination: {
+			el: '.sw-latest__pg',
+			clickable: true,
+		},
+		navigation: {
+			prevEl: '.sw-latest__prev',
+			nextEl: '.sw-latest__next',
+		},
 	});
 
-	function changeGroup(_num) {
-        if(_num == 1) {
-            sw_latest.slidesPerGroup = 2;
-        }
-		else {
-            sw_latest.slidesPerGroup = 3;
-        }
-    }
+	$('.sw-latest .swiper-wrapper').mouseenter(function() {
+		$('.sw-latest__prev').stop().fadeIn(300);
+		$('.sw-latest__next').stop().fadeIn(300);
+	});
+	$('.sw-latest .swiper-wrapper').mouseleave(function() {
+		$('.sw-latest__prev').stop().fadeOut(300);
+		$('.sw-latest__next').stop().fadeOut(300);
+	});
 }

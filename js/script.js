1259669d43303a2bfdd7bcc9996daee6a41a591a
jskimweb <jskimweb@gmail.com>
2021-09-11 18:39:47 +0900

    Add a '.elec'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
 window.onload = function () {
+	// go-top
+	$(window).scroll(function () {
+		var sT = $(window).scrollTop();
+		if (sT >= 400) {
+			$('.go-top').addClass('go-top--show');
+		} else {
+			$('.go-top').removeClass('go-top--show');
+		}
+	})
+	$('.go-top').click(function () {
+		$('html,body').animate({
+			scrollTop: 0
+		});
+	});
+
+	// elec show & hide
+	function elecHide() {
+		$('.elec').stop().slideUp(300);
+	}
+
+	$('.elec-btn').mouseenter(function () {
+		$('.elec').stop().slideDown(300);
+		clearTimeout(elec_timer);
+	});
+
+	$('.elec-btn').mouseleave(function () {
+		elec_timer = setTimeout(elecHide, 50);
+	});
+
+	$('.elec').mouseenter(function() {
+		clearTimeout(elec_timer);
+	})
+
+	$('.elec').mouseleave(function () {
+		elec_timer = setTimeout(elecHide, 50);
+	});
+
 	// THE LATEST slide
 	new Swiper('.sw-latest', {
 		slidesPerView: "auto",
@@ -13,12 +50,15 @@ window.onload = function () {
 		},
 	});
 
-	$('.sw-latest .swiper-wrapper').hover(function () {
-		$('.sw-latest__prev').stop().fadeIn(300);
-		$('.sw-latest__next').stop().fadeIn(300);
-	}, function () {
-		$('.sw-latest__prev').stop().fadeOut(300);
-		$('.sw-latest__next').stop().fadeOut(300);
+	// THE LATEST navigation show & hide
+	$('.sw-latest').mouseenter(function () {
+		$('.sw-latest__prev').addClass('sw-latest__prev--show');
+		$('.sw-latest__next').addClass('sw-latest__next--show');
+	});
+
+	$('.sw-latest').mouseleave(function () {
+		$('.sw-latest__prev').removeClass('sw-latest__prev--show');
+		$('.sw-latest__next').removeClass('sw-latest__next--show');
 	});
 
 	// LATEST NEWS slide
@@ -28,7 +68,7 @@ window.onload = function () {
 		allowTouchMove: false,
 	});
 
-	// Add-on show/hide
+	// Add-on show & hide
 	addon_top = $('.addon-top');
 	addon_icon = addon_top.find('i');
 	addon_main = $('.addon-main');
@@ -38,37 +78,33 @@ window.onload = function () {
 		addon_icon.toggleClass('icon-down-micro');
 	});
 
-	// sns-box show/hide
-	$('.footer__sns--y').mouseenter(function () {
-		var cont = $(this).attr('sns-data');
-
-		$(cont).show();
-		$('.sns-box').show();
-	})
-	$('.footer__sns--y').mouseleave(function () {
-		$('.sns-box-cont').hide();
-		$('.sns-box').hide();
-	})
-
-	$('.footer__sns--f').mouseenter(function () {
+	// sns-box show & hide
+	$('.footer__sns--y').hover(function () {
 		var cont = $(this).attr('sns-data');
 		$(cont).show();
 		$('.sns-box').show();
-	})
-	$('.footer__sns--f').mouseleave(function () {
+	}, function () {
 		$('.sns-box-cont').hide();
 		$('.sns-box').hide();
-	})
+	});
 
-	$('.footer__sns--i').mouseenter(function () {
+	$('.footer__sns--f').hover(function () {
 		var cont = $(this).attr('sns-data');
 		$(cont).show();
 		$('.sns-box').show();
-	})
-	$('.footer__sns--i').mouseleave(function () {
+	}, function () {
 		$('.sns-box-cont').hide();
 		$('.sns-box').hide();
-	})
+	});
+
+	$('.footer__sns--i').hover(function () {
+		var cont = $(this).attr('sns-data');
+		$(cont).show();
+		$('.sns-box').show();
+	}, function () {
+		$('.sns-box-cont').hide();
+		$('.sns-box').hide();
+	});
 
 	// sns-box close
 	$('.footer__sns--y').find('.icon-cancel').click(function () {
